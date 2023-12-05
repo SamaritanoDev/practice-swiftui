0a840c81b79a44bc44d3ebed988da42e571fbd63
Flutterina Mobile <91768959+SamaritanoDev@users.noreply.github.com>
2023-12-04 22:43:54 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lesly Samaritano on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
